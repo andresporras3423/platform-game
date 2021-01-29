d150a91139531c085778728c8e17f1a7cddefe0c
oscarporras02 <oscarrussi@outlook.com>
2021-01-28 19:55:59 -0500

    edit credits to show my linkedin and portfolio link

--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -11,9 +11,16 @@ export default class CreditsScene extends Phaser.Scene {
 
   create() {
     this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#000' });
-    this.madeByText = this.add.text(0, 0, 'Created By: Oscar Andrés Russi Porras', { fontSize: '26px', fill: '#000' });
-    this.arrobaText = this.add.text(0, 0, 'oscarrussi@outlook.com', { fontSize: '26px', fill: '#000' });
+    this.madeByText = this.add.text(0, 0, 'Created By: Oscar Andrés Russi Porras', { fontSize: '24px', fill: '#000' });
+    this.emailText = this.add.text(0, 0, 'Email: oscarrussi@outlook.com', { fontSize: '24px', fill: '#000' });
+    this.linkedinText = this.add.text(0, 0, 'LinkedIn: linkedin.com/in/oscar-andres-russi-porras/', { fontSize: '24px', fill: '#000' });
+    this.portfolioText = this.add.text(0, 0, 'Portfolio: andresporras3423.github.io/my-portfolio/', { fontSize: '24px', fill: '#000' });
     this.zone = this.add.zone(config.width / 2, config.height / 2, config.width, config.height);
+
+    Phaser.Display.Align.In.Center(
+      this.creditsText,
+      this.zone,
+    );
 
     Phaser.Display.Align.In.Center(
       this.creditsText,
@@ -26,12 +33,19 @@ export default class CreditsScene extends Phaser.Scene {
     );
 
     Phaser.Display.Align.In.Center(
-      this.arrobaText,
+      this.madeByText,
+      this.zone,
+    );
+
+    Phaser.Display.Align.In.Center(
+      this.emailText,
       this.zone,
     );
 
     this.madeByText.setY(600);
-    this.arrobaText.setY(1200);
+    this.emailText.setY(900);
+    this.linkedinText.setY(1200);
+    this.portfolioText.setY(1500);
 
     this.creditsTween = this.tweens.add({
       targets: this.creditsText,
@@ -55,14 +69,38 @@ export default class CreditsScene extends Phaser.Scene {
       }.bind(this),
     });
 
-    this.arrobaTween = this.tweens.add({
-      targets: this.arrobaText,
+    this.emailTween = this.tweens.add({
+      targets: this.emailText,
       y: -300,
       ease: 'Power1',
       duration: 8000,
       delay: 1000,
       onComplete: function () {
-        this.arrobaTween.destroy;
+        this.emailTween.destroy;
+        this.scene.start('Title');
+      }.bind(this),
+    });
+
+    this.linkedinTween = this.tweens.add({
+      targets: this.linkedinText,
+      y: -300,
+      ease: 'Power1',
+      duration: 8000,
+      delay: 1000,
+      onComplete: function () {
+        this.emailTween.destroy;
+        this.scene.start('Title');
+      }.bind(this),
+    });
+
+    this.portfolioTween = this.tweens.add({
+      targets: this.portfolioText,
+      y: -300,
+      ease: 'Power1',
+      duration: 8000,
+      delay: 1000,
+      onComplete: function () {
+        this.emailTween.destroy;
         this.scene.start('Title');
       }.bind(this),
     });
