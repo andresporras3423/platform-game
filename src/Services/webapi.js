3d28c3bba701c80394fe358ed8366eee0f6045a2
oscarporras02 <oscarrussi@outlook.com>
2021-02-14 06:53:56 -0500

    add suggestion of TSE, using try catch in asyn methods

--- src/Services/webapi.js
+++ src/Services/webapi.js
@@ -6,32 +6,40 @@ const webapi = (() => {
     3: 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Vd5SKbQfAzVOc48DZkxP/scores/',
   };
   const getScore = async () => {
-    const response = await fetch(urls[gameOptions.jumps], {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    });
-    const data = await response.json();
-    const bestScores = data.result.sort((a, b) => b.score - a.score);
-    return bestScores.slice(0, 10);
+    try {
+      const response = await fetch(urls[gameOptions.jumps], {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      const bestScores = data.result.sort((a, b) => b.score - a.score);
+      return bestScores.slice(0, 10);
+    } catch {
+      return -1;
+    }
   };
 
   const saveScore = async (name, score) => {
-    const response = await fetch(urls[gameOptions.jumps], {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        user: name,
-        score,
-      }),
-    });
-    const data = await response.json();
-    return data;
+    try {
+      const response = await fetch(urls[gameOptions.jumps], {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: name,
+          score,
+        }),
+      });
+      const data = await response.json();
+      return data;
+    } catch {
+      return -1;
+    }
   };
 
   return { getScore, saveScore };
